f0525944386282aee6075fb175a9afbe73780a28
Achille Poirier <achillepoirier01@gmail.com>
2025-02-26 14:52:12 +0100

    correction user, ajout table inscription user à event

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,15 @@
 from config import db
-
+from app.associations.event_users import event_users
 
 class User(db.Model):
-
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    firstname = db.Column(db.String(200), nullable=False)
+    familyname = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.String(200), nullable=False)
+    age = db.Column(db.String(200), nullable=False)
+    events = db.relationship('Event', secondary=event_users, back_populates='users')
