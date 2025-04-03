11ecb97e8af898bafa8e04dae0082c3f99a837bf
Janov <achillepoirier01@gmail.com>
2025-04-03 19:30:35 +0200

    FIX test non fonctionels

--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -1,11 +1,22 @@
 import pytest
+from app import create_app
+import os
 from flask import Flask
-from app.routes.app_routes import main_bp  # Adjust this import based on your project structure
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+from app.routes.app_routes import main_bp
+from app.routes.team_routes import team_bp
+from app.routes.user_routes import auth_bp
+from app.routes.event_routes import event_bp
+from app.routes.sport_routes import sport_bp
+from config import Config, db
+from flasgger import Swagger
+from flask_cors import CORS
 
 @pytest.fixture
 def app():
-    app = Flask(__name__)
-    app.register_blueprint(main_bp)  # Register your blueprint
+    app = create_app()
     return app
 
 @pytest.fixture
