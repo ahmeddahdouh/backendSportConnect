c7b1d04d29801334f796060bc760c038e360f2fe
ahmed <ahmed.dahdouh98@gmail.com>
2025-04-21 00:51:11 +0200

    Refactor: Apply Clean Architecture to all users endpoints Apply Black code formatting to the entire application to ensure consistent structure

--- app/controllers/user_routes.py
+++ app/controllers/user_routes.py
@@ -1,14 +1,13 @@
 import os
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from app.models.user import User
-from app.models.event import Event
-from app.associations.event_users import EventUsers
+from ..associations.user_sports import UserSports
+from ..models import Sport
+from ..services.user_service import UserService
 from config import db
 from sqlalchemy.exc import IntegrityError
 from flask import (
     request,
-    abort,
-    make_response,
     jsonify,
     Blueprint,
     send_from_directory,
@@ -21,9 +20,7 @@ import json
 from datetime import timedelta
 import uuid
 
-from ..associations.user_sports import UserSports
-from ..models import Sport
-from ..services.user_service import UserService
+
 
 auth_bp = Blueprint("auth", __name__)
 user_service = UserService()
@@ -32,74 +29,16 @@ user_service = UserService()
 @auth_bp.route("/register", methods=["POST"])
 @swag_from("../../static/docs/add_user_docs.yaml")
 def register():
-    data = request.get_json()
-    # Récupération des champs requis
-    username = data.get("username")
-    email = data.get("email")
-    password = data.get("password")
-    confirm_password = data.get("confirmPassword")
-    firstname = data.get("firstname")
-    familyname = data.get("familyname")
-    city = data.get("city")
-    phone = data.get("phone")
-    age = data.get("age")
-
-    # Vérification des champs obligatoires
-    if not all(
-        [
-            username,
-            email,
-            password,
-            confirm_password,
-            firstname,
-            familyname,
-            city,
-            phone,
-            age,
-        ]
-    ):
-        return jsonify({"message": "Missing data"}), 400
-
-    # Vérification de la correspondance des mots de passe
-    if password != confirm_password:
-        return jsonify({"message": "Passwords do not match"}), 400
-
-    # Vérification que l'âge est bien un entier positif
     try:
-        age = int(age)
-        if age <= 0:
-            return jsonify({"message": "Invalid age"}), 400
-    except ValueError:
-        return jsonify({"message": "Age must be an integer"}), 400
-
-    # Hachage du mot de passe
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode(
-        "utf-8"
-    )
-
-    # Création de l'utilisateur
-    user = User(
-        username=username,
-        email=email,
-        password=hashed_password,
-        firstname=firstname,
-        familyname=familyname,
-        city=city,
-        phone=phone,
-        age=age,
-    )
-
-    try:
-        # Tentative d'ajout de l'utilisateur à la base de données
-        db.session.add(user)
-        db.session.commit()
-        return jsonify({"message": "User registered successfully"}), 201
-    except IntegrityError:
-        db.session.rollback()
-        return jsonify({"message": "Username or email already exists"}), 409
+        user_data = request.get_json()  # on récupère les données JSON
+        response = user_service.create_user(user_data)
+        return jsonify(response), 201
+    except ValueError as e:
+        # Message d'erreur spécifique
+        return jsonify({"error": str(e)}), 409  # Code HTTP 409 pour conflit
     except Exception as e:
-        db.session.rollback()
-        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+        # Message d'erreur générique
+        return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500  # Erreur serveur
 
 
 @auth_bp.route(
@@ -197,52 +136,18 @@ def delete_sport(sport_id):
 
 
 @auth_bp.route(
-    "/users/<int:user_id>", methods=["PUT"]
-)  # modification des informations de l'utilisateur
+    "/users/<int:user_id>", methods=["PUT"])  # modification des informations de l'utilisateur
 def update_user(user_id):
     data = request.get_json()
-
-    # Récupérer l'utilisateur
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"message": "User not found"}), 404
-
-    # Mise à jour des champs s'ils sont fournis
-    if "username" in data:
-        user.username = data["username"]
-    if "email" in data:
-        user.email = data["email"]
-    if "firstname" in data:
-        user.firstname = data["firstname"]
-    if "familyname" in data:
-        user.familyname = data["familyname"]
-    if "city" in data:
-        user.city = data["city"]
-    if "phone" in data:
-        user.phone = data["phone"]
-    if "age" in data:
-        try:
-            user.age = int(data["age"])
-            if user.age <= 0:
-                return jsonify({"message": "Invalid age"}), 400
-        except ValueError:
-            return jsonify({"message": "Age must be an integer"}), 400
-    if "password" in data:
-        hashed_password = bcrypt.hashpw(
-            data["password"].encode("utf-8"), bcrypt.gensalt()
-        ).decode("utf-8")
-        user.password = hashed_password
-
     try:
-        # Sauvegarde en base de données
-        db.session.commit()
-        return jsonify({"message": "User updated successfully"}), 200
-    except IntegrityError:
-        db.session.rollback()
-        return jsonify({"message": "Username or email already exists"}), 409
+            user_service.update_user(data, user_id)
+            return jsonify({"message": "User updated successfully."}), 200
+    except ValueError as e:
+        return jsonify({"erreur" : str(e)}),404
+    except IntegrityError as e:
+        return jsonify({"erreur":str(e)}),409
     except Exception as e:
-        db.session.rollback()
-        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+        return jsonify({"erreur":str(e)}),500
 
 
 @auth_bp.route("/login", methods=["POST"])
@@ -250,39 +155,21 @@ def login():
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
+
     if not username or not password:
         return jsonify({"message": "Missing data"}), 400
-    user = User.query.filter_by(username=username).first()
-    if user:
-        if bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
-            access_token = create_access_token(
-                identity=json.dumps(
-                    {
-                        "username": user.username,
-                        "id": user.id,
-                        "profileImage": user.profileImage,
-                    }
-                ),
-                expires_delta=timedelta(hours=1),
-            )
-            return jsonify(access_token=access_token), 200
-        else:
-            return jsonify({"message": "Invalid credentials"}), 401
-    else:
-        return jsonify({"message": "User does not exist"}), 401
 
+    result, status = user_service.login(username, password)
+    return jsonify(result), status
 
 @auth_bp.route("/users", methods=["GET"])
 @jwt_required()
 def get_users():
-    users = User.query.all()
-    print(users)
-    return jsonify([row2dict(user) for user in users])
-
+    users = user_service.get_users()
+    return jsonify(users), 200
 
 @auth_bp.route("/uploads/<filename>", methods=["GET"])
 def uploaded_file(filename):
-    print(current_app.config["UPLOAD_FOLDER"])
     return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
 
 
@@ -291,23 +178,16 @@ def uploaded_file(filename):
 def update_profile_image():
     if "file" not in request.files:
         return jsonify({"message": "Missing file"}), 400
-
     current_user = get_jwt_identity()
     current_user_json = json.loads(current_user)
-
     file = request.files["file"]
     if file.filename == "":
         return jsonify({"message": "Missing file"}), 400
-    file_ext = file.filename.split(".")[-1]
-    unique_id = uuid.uuid4()
-    profileImageName = str(unique_id) + "." + file_ext
-    file_path = os.path.join(current_app.config["UPLOAD_FOLDER"], profileImageName)
-    file.save(file_path)
-    user = User.query.get(current_user_json["id"])
-    user.profileImage = profileImageName
-    db.session.commit()
-
-    return jsonify({"image": user.profileImage}), 201
+    try :
+        profile_image = user_service.update_profile_image(current_user_json["id"], file)
+        return jsonify({"image": profile_image }), 201
+    except Exception as e:
+        return jsonify({"erreur":str(e)}),500
 
 
 @auth_bp.route("/users/<int:user_id>", methods=["GET"])
