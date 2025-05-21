2c245056c458b37b1434f5e097d3dd0d193a6a82
seck <main>
2025-05-21 17:46:33 +0200

    ia intro la BD

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -3,11 +3,12 @@ import numpy as np
 import tensorflow as tf
 import pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences  
-from app.models.donne_IA.dataset_sport import data,sport_list
-from app.repositories.event_repository import EventRepository  # ← à adapter si besoin
+from app.models.donne_IA.dataset_sport import data, sport_list
+from app.repositories.event_repository import EventRepository
 
 
-event=EventRepository()
+event = EventRepository()
+
 # Charger le modèle et le tokenizer
 model = tf.keras.models.load_model("app/models/donne_IA/model.h5")
 
@@ -21,20 +22,27 @@ for item in data:
         unique_answers.append(item['answer'])
 
 
-
-
 def predict_category(question, threshold=0.4):
     seq = tokenizer.texts_to_sequences([question])
     pad = pad_sequences(seq, maxlen=10, padding='post')
     pred = model.predict(pad)[0]
     idx = np.argmax(pred)
     if pred[idx] < threshold:
-        return "Je ne suis pas sûr de comprendre. Peux-tu reformuler ?", None
+        return "Je ne suis pas sûr de comprendre. Peux-tu reformuler ?", []
+
     answer = unique_answers[idx]
     sport = extract_sport_de_answer(answer)
-    descrip,events=get_events_description(sport)
-   
-    return answer,descrip
+
+    if sport:
+        descriptions = event.get_events_description_by_sport(sport)
+        if descriptions:
+            message = f"{answer}\n\n\nVoici la liste des événements liés à {sport} dans notre base :"
+            return message, descriptions
+        else:
+            message = f"{answer}\n\nAucun événement trouvé pour le sport : {sport} dans notre base."
+            return message, []
+    else:
+        return answer, []
 
 
 def extract_sport_de_answer(answer):
@@ -43,13 +51,3 @@ def extract_sport_de_answer(answer):
         if sport in answer_lower:
             return sport
     return None
-
-
-def get_events_description(question):
-    sport = extract_sport_de_answer(question)
-    if sport:
-        events = event.get_events_by_sport(sport)
-        description = event.get_events_description_by_sport(sport)
-        return description,events
-    else:
-        return None
